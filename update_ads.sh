22559b555c34a77b16cf06cf40d778b2a851761e
Nick Templeton <nick@nicktempleton.com>
2023-04-17 15:14:22 -0500

    Fix typo in output filename

--- update_ads.sh
+++ update_ads.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-OUTFILE="/var/unbound/etc/adservers.com"
+OUTFILE="/var/unbound/etc/adservers.conf"
 SERVERLIST_URL="http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext&useip=127.0.0.1"
 
 ftp -o "$OUTFILE" "$SERVERLIST_URL"
