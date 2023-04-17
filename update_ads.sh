724b83face078bca674a88aed0ee72c969877d08
Nick Templeton <nick@nicktempleton.com>
2023-04-17 15:40:55 -0500

    Use HTTPS to skip a redirect

--- update_ads.sh
+++ update_ads.sh
@@ -3,7 +3,7 @@
 set -e
 
 OUTFILE="/var/unbound/etc/adservers.conf"
-SERVERLIST_URL="http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext&useip=127.0.0.1"
+SERVERLIST_URL="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext&useip=127.0.0.1"
 
 ftp -o "$OUTFILE" "$SERVERLIST_URL"
 
