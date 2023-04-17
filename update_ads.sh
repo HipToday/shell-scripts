e0c1cfdc94774d92c604832a19b16474573190b2
Nick Templeton <nick@nicktempleton.com>
2023-04-17 14:51:36 -0500

    Script to fetch list of ad servers for DNS ad-blocking

---
+++ update_ads.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -e
+
+OUTFILE="/var/unbound/etc/adservers.com"
+SERVERLIST_URL="http://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext&useip=127.0.0.1"
+
+ftp -o "$OUTFILE" "$SERVERLIST_URL"
+
+rcctl reload unbound
