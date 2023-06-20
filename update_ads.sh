eda21c245dce9b127b7e1da614f7f2e141291e5d
Nick Templeton <nick@nicktempleton.com>
2023-06-20 11:21:28 -0500

    Whitelist ipstack.com so that AceHardware.com will work properly on mobile browser.

--- update_ads.sh
+++ update_ads.sh
@@ -7,7 +7,7 @@ OUTFILE="/var/unbound/etc/adservers.conf"
 # IP address to redirect ad domains to
 USEIP="127.0.0.1"
 # Comma-separated whitelist of domains (https://pgl.yoyo.org/as/formats.php#skip)
-SKIP="click.redditmail.com"
+SKIP="click.redditmail.com,ipstack.com"
 SERVERLIST_URL="https://pgl.yoyo.org/adservers/serverlist.php?hostformat=unbound&showintro=0&mimetype=plaintext&useip=$USEIP&skip=$SKIP"
 
 ftp -o "$OUTFILE" "$SERVERLIST_URL"
